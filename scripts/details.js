1acb6abfa1ab66b83b5c48bd7c25b948e1adc5dc
VinoliaMoekwa <moe23001@byui.edu>
2026-04-14 15:12:19 +0200

    fetch details error

--- scripts/details.js
+++ scripts/details.js
@@ -21,16 +21,21 @@ async function fetchDramaDetails(dramaId) {
 
 async function fetchTrailer(dramaName) {
   const query = `${dramaName} official trailer kdrama`;
-  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=1&q=${encodeURIComponent(query)}&key=${YOUTUBE_API_KEY}`;
+
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=5&q=${encodeURIComponent(query)}&videoEmbeddable=true&videoSyndicated=true&key=${YOUTUBE_API_KEY}`;
 
   const response = await fetch(url);
+
   if (!response.ok) {
     throw new Error("Failed to fetch trailer");
   }
 
   const data = await response.json();
+  console.log("YouTube trailer data:", data);
+
   return data.items && data.items.length > 0 ? data.items[0] : null;
 }
+
 
 function renderDetails(drama, trailer) {
   const poster = drama.poster_path
